f49e6fa3dabaa19f4497aa1682324f4b82b684a6
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2022-12-28 11:02:54 +0300

    update on about

--- src/controller/about.controller.js
+++ src/controller/about.controller.js
@@ -18,8 +18,8 @@ const getAbout = asyncHandler(async(req,res)=>{
 
 const deleteAbout = asyncHandler(async(req,res)=>{
     const currentAbout= await AboutModel.find({aboutId:req.params.id});
-      await AboutModel.deleteOne({userId:req.params.id});
-      res.send(`About ${currentHero[0].userId} removed`);
+      await AboutModel.deleteOne({aboutId:req.params.id});
+      res.send(`About ${currentAbout[0].aboutId} removed`);
 })
 
 module.exports = {
