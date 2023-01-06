74610690bb0f8fc307c697aa28f136f4189a122c
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-01-06 15:19:39 +0300

    updated

--- src/controller/order.controller.js
+++ src/controller/order.controller.js
@@ -3,12 +3,11 @@ const { v4: uuidv4 } = require('uuid')
 const OrderModel = require("../models/order.model")
 const CustomerModel = require("../models/customer.model");
 
+const getOrders = asyncHandler(async (req, res) => {
+    const orders = await OrderModel.find();
 
-const getOrders = asyncHandler(async(req,res)=>{
-    const orders = await OrderModel.find();   
-    
-    
-    const resut =  {
+
+    const resut = {
         data: orders
     }
     res.status(200).send(resut)
@@ -23,14 +22,24 @@ const orderProduct = asyncHandler(async (req, res) => {
         name: req.body.name,
         phoneNumber: req.body.phoneNumber,
     });
-//     const order = await OrderModel.find();
-//     const o = order.filter(
-//         (i) => i.productId === productId
-//     )
-//    o.filter(
-//     (i)
-//    )
-    
+    const order = await OrderModel.find({ productId: productId });
+    // console.log("Order", order)
+    const Cquantity = 0;
+    order.map((booked) => {
+        const bookedStartDate = new Date(booked.startDate);
+        const bookedEndDate = new Date(booked.endDate);
+        const newEndDate = new Date(endDate);
+        const newStartDate = new Date(startDate)
+
+        if (bookedEndDate.getTime() >= newStartDate.getTime()
+            && bookedEndDate.getTime() <= newEndDate.getTime()
+            || bookedStartDate.getTime() >= newStartDate.getTime() 
+            && bookedStartDate.getTime() <= newEndDate.getTime()) {
+            console.log("Booked", booked)
+            Cquantity += booked.quantity
+        }
+    })
+
     const orderId = uuidv4();
     const reservation = new OrderModel({
         orderId: orderId,
@@ -40,7 +49,7 @@ const orderProduct = asyncHandler(async (req, res) => {
         productId: req.body.productId,
         startDate: req.body.startDate,
         endDate: req.body.endDate,
-        quntity:req.body.quantity,
+        quantity: req.body.quantity,
     });
     customer.save();
     reservation.save().then(result => {
@@ -56,8 +65,9 @@ const orderProduct = asyncHandler(async (req, res) => {
                 startDate: result.startDate,
                 endDate: result.endDate,
                 status: result.status,
-                email:result.email,
-                availablity:result.availablity,
+                quantity:result.quantity,
+                email: result.email,
+                availablity: result.availablity,
             }
         })
     }).catch * (err => {
