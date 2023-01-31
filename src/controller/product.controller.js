ede9fbc233d9e950ba522e7a3ddbdd22b43b435b
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-01-31 11:00:32 +0300

    all products route

--- src/controller/product.controller.js
+++ src/controller/product.controller.js
@@ -7,16 +7,37 @@ const GensetModel = require("../models/genset.model");
 const AtsModel = require("../models/ats.model");
 
 const getProducts = asyncHandler(async(req,res)=>{
-    const generator = await GeneratorModel.find();
-    const pump = await PumpModel.find();
-    const sparePart = await SparePartModel.find();
-    const genset = await GensetModel.find();
-    const ats = await AtsModel.find();
-
+    let products = [];
+    const generator = await GeneratorModel.find().then((result)=>{
+        result.map((product)=>{
+            products.push(product);
+        })
+    });
+    const pump = await PumpModel.find().then((result)=>{
+        result.map((product)=>{
+            products.push(product);
+        })
+    });
+    const sparePart = await SparePartModel.find().then((result)=>{
+        result.map((product)=>{
+            products.push(product);
+        })
+    });;
+    const genset = await GensetModel.find().then((result)=>{
+        result.map((product)=>{
+            products.push(product);
+        })
+    });;
+    const ats = await AtsModel.find().then((result)=>{
+        result.map((product)=>{
+            products.push(product);
+        })
+    });;
 
     const resut =  { 
-        data: generator  + pump + sparePart + genset + ats
+        data: products 
     }
+    // console.log(resut)
     res.status(200).send(resut)
 });
 
