804d8df6d46dca23feba6361c183412d6cc29f48
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-03-14 19:35:17 +0300

    updated

--- src/config/db_config.js
+++ src/config/db_config.js
@@ -2,26 +2,23 @@ const { printConsole } = require("../utils/development");
 
 const mongoose = require("mongoose");
 const MONGO_DB_URI =
-  process.env.MONGO_REMOTE_URL || process.env.MONGO_LOCAL_URL;
+  process.env.MONGO_REMOTE_URL;
 
 const connectToDB = async () => {
   try {
     printConsole(
+
       { data: "Connecting to MongoDB ......" },
       { printLocation: "db_config.js:12" },
       { textColor: "yellow" }
     );
-    // mongodb+srv://melan-Ethiopia:<password>@cluster0.fnmxl2y.mongodb.net/?retryWrites=true&w=majority
-    // "mongodb+srv://admin:bnadminpass@cluster0.ze5z9gi.mongodb.net/
-    mongoose.set('strictQuery', false);
     const DBConnection = await mongoose.connect("mongodb+srv://melan-Ethiopia:melanE1234@cluster0.fnmxl2y.mongodb.net/", {
       useNewUrlParser: true,
-      // useCreateIndex: true,
       useUnifiedTopology: true,
-      // useFindAndModify: false,
     });
 
     printConsole(
+      
       { data: `Database Connected : ${DBConnection.connection.host}` },
       { printLocation: "db_config.js:24" },
       {
@@ -34,5 +31,8 @@ const connectToDB = async () => {
     process.exit(1);
   }
 };
+
+  // mongodb+srv://melan-Ethiopia:<password>@cluster0.fnmxl2y.mongodb.net/?retryWrites=true&w=majority
+    // "mongodb+srv://admin:bnadminpass@cluster0.ze5z9gi.mongodb.net/
 
 module.exports = connectToDB;
