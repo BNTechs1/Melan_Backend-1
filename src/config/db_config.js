83bbac882cad5bae4f2a287a3795f96c8df455fe
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-03-14 18:48:37 +0300

    update

--- src/config/db_config.js
+++ src/config/db_config.js
@@ -13,6 +13,7 @@ const connectToDB = async () => {
     );
     // mongodb+srv://melan-Ethiopia:<password>@cluster0.fnmxl2y.mongodb.net/?retryWrites=true&w=majority
     // "mongodb+srv://admin:bnadminpass@cluster0.ze5z9gi.mongodb.net/
+    mongoose.set('strictQuery', false);
     const DBConnection = await mongoose.connect("mongodb+srv://melan-Ethiopia:melanE1234@cluster0.fnmxl2y.mongodb.net/", {
       useNewUrlParser: true,
       // useCreateIndex: true,
