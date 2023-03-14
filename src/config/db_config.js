e17b214cc2b64c1eefab10c8bce6939eefd6661c
Ermias Asmare <61870061+jrermi12@users.noreply.github.com>
2023-03-14 10:06:56 +0300

    updated .env

--- src/config/db_config.js
+++ src/config/db_config.js
@@ -12,7 +12,7 @@ const connectToDB = async () => {
       { textColor: "yellow" }
     );
     // 
-    const DBConnection = await mongoose.connect("mongodb+srv://admin:bnadminpass@cluster0.ze5z9gi.mongodb.net/", {
+    const DBConnection = await mongoose.connect("mongodb+srv://melan-Ethiopia:Mems@2015@cluster0.fnmxl2y.mongodb.net/", {
       useNewUrlParser: true,
       // useCreateIndex: true,
       useUnifiedTopology: true,
